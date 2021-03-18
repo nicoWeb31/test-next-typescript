1c743db945fcf62020da785682187190a06cafec
nicoWeb31 <nico.riot@free.fr>
2021-03-18 20:01:53 +0100

    event data home page

--- pages/event/index.tsx
+++ pages/event/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { EventList } from '../../components/event/event-list';
+import { getFeaturedEvents } from '../../dummy-data';
+import { Event } from '../../interfaces/envent';
 
 const allEventPage : React.FC = () => {
+
+    const featuredEvent : Event[] = getFeaturedEvents();
+
     return (
         <div>
-            <h3>All events pages</h3>
+            <EventList events={featuredEvent}/>
+
         </div>
     )
 }
