1d495d4d994c85c8f17907a7051b7d37e0f5c72d
nicoWeb31 <nico.riot@free.fr>
2021-03-25 14:04:16 +0100

    no prerender with useSWR

--- pages/last-sale/index.tsx
+++ pages/last-sale/index.tsx
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from "react";
+import useSWR from 'swr';
 
 interface LastSalePageProps {}
-interface Albums {
+interface Album {
     userId: string;
     id: string;
     title: string;
 }
 
 const LastSalePage: React.FC = () => {
-    const [album, setAlbum] = useState<Albums[] | null>(null);
-    const [isloading, setIsLoading] = useState<boolean>(false);
+    // const [album, setAlbum] = useState<Albums[] | null>(null);
+    // const [isloading, setIsLoading] = useState<boolean>(false);
 
-    useEffect(() => {
-        setIsLoading(true);
-        fetch("https://jsonplaceholder.typicode.com/albums")
-            .then((response) => response.json())
-            .then((data) => {
-                setAlbum(data);
-                setIsLoading(false);
-            });
-    }, []);
 
-    console.log(album);
+    const {data, error} =useSWR("https://jsonplaceholder.typicode.com/albums");
 
-    if(!album) {
+
+
+    // useEffect(() => {
+    //     setIsLoading(true);
+    //     fetch("https://jsonplaceholder.typicode.com/albums")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             setAlbum(data);
+    //             setIsLoading(false);
+    //         });
+    // }, []);
+
+    // console.log(album);
+
+    if(error) {
+        console.log(error)
+    }
+
+    if(!data) {
         return <p>loading...</p>;
     }
 
     return (
         <ul>
-            {album?.map((alb) => {
+            {data?.map((alb: Album) => {
                 return <li key={alb.id}>{alb.title}</li>;
             })}
         </ul>
