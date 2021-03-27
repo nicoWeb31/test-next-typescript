7786c755d106f5aaaa695ba3dd594bd69c0f2f10
nicoWeb31 <nico.riot@free.fr>
2021-03-27 10:35:31 +0100

    fetch from api

--- pages/api/feedBack.tsx
+++ pages/api/feedBack.tsx
@@ -3,34 +3,45 @@ import type * as E from "express";
 import fs from "fs";
 import path from "path";
 
-
+const feedBackPath = () => path.join(process.cwd(), "data", "feedbach.json");
+const extractFeedback = (pathname: string) => {
+    const fileData = fs.readFileSync(pathname);
+    return JSON.parse(fileData.toString());
+};
 
 const handler = function (req: E.Request, res: E.Response) {
     if (req.method === "GET") {
-        return res.status(200).json({ message: "this works" });
+        const path = feedBackPath();
+        const data = extractFeedback(path);
+        return res.status(200).json({ feedBack: data });
     }
 
     if (req.method === "POST") {
         // const body = JSON.stringify(req.body);
         // console.log(body);
 
-        const {email, text} = req.body;
+        const { email, text } = req.body;
 
         const newFeedback = {
             id: Date.now().toString(),
             email,
-            text
-        }
-        console.log("🚀 ~ file: feedBack.tsx ~ line 24 ~ handler ~ newFeedback", newFeedback)
+            text,
+        };
+        console.log(
+            "🚀 ~ file: feedBack.tsx ~ line 24 ~ handler ~ newFeedback",
+            newFeedback
+        );
 
         console.log(newFeedback);
-        const pathname = path.join(process.cwd(), 'data', 'feedbach.json');
-        const fileData = fs.readFileSync(pathname);
-        const data = JSON.parse(fileData.toString());
+        const pathname = feedBackPath();
+
+        const data = extractFeedback(pathname);
         data.push(newFeedback);
         fs.writeFileSync(pathname, JSON.stringify(data));
 
-        return res.status(201).json({message: 'create with sucess!', newFeedback});
+        return res
+            .status(201)
+            .json({ message: "create with sucess!", newFeedback });
     }
 };
 
--- pages/tryapi/index.tsx
+++ pages/tryapi/index.tsx
@@ -1,22 +1,33 @@
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 import axios from "axios";
 
 const TryApi = () => {
+    const [feedBacks, setFeedback] = useState([]);
 
     const emailInputRef = useRef<HTMLInputElement>(null);
     const feedBackInputRef = useRef<HTMLTextAreaElement>(null);
 
-    const submitHandler = async (e :React.FormEvent)=>{
+    const submitHandler = async (e: React.FormEvent) => {
         e.preventDefault();
         const email = emailInputRef.current?.value;
         const text = feedBackInputRef.current?.value;
 
-        const body = {email, text};
-        const headers = {headers : {'Content-Type': 'application/json'}};
+        const body = { email, text };
+        const headers = { headers: { "Content-Type": "application/json" } };
 
-        const response = await axios.post('/api/feedBack',JSON.stringify(body),headers);
-        console.log(response)
-    }
+        const response = await axios.post(
+            "/api/feedBack",
+            JSON.stringify(body),
+            headers
+        );
+        console.log(response);
+    };
+
+    const handleFetchData = async () => {
+        const { data } = await axios.get("/api/feedBack");
+        console.log(data.feedBack);
+        setFeedback(data.feedBack);
+    };
 
     return (
         <div>
@@ -24,14 +35,36 @@ const TryApi = () => {
             <form onSubmit={submitHandler}>
                 <div>
                     <label htmlFor="email">Your Email</label>
-                    <input type="email" id="email" ref={emailInputRef} name="email"/>
+                    <input
+                        type="email"
+                        id="email"
+                        ref={emailInputRef}
+                        name="email"
+                    />
                 </div>
                 <div>
                     <label htmlFor="feedBack">Your Feddback</label>
-                    <textarea id="feedBack"  rows={5} ref={feedBackInputRef} name="text"></textarea>
+                    <textarea
+                        id="feedBack"
+                        rows={5}
+                        ref={feedBackInputRef}
+                        name="text"
+                    ></textarea>
                 </div>
                 <button>send feedBack</button>
             </form>
+
+            <hr/>
+
+            <div>
+                <h3 className="center">load feedBack </h3>
+                <div className="center">
+                    <button onClick={handleFetchData}>clik</button>
+                </div>
+                <ul>
+                    {feedBacks.map((feedBack: any) =><li key={feedBack.id}>{feedBack.text}</li>)}
+                </ul>
+            </div>
         </div>
     );
 };
