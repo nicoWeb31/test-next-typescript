4c0d596bc28744302038dfe8cd7b66f5b2524fa7
nicoWeb31 <nico.riot@free.fr>
2021-03-30 11:06:15 +0200

    add mongodb

--- pages/api/newsletter/index.tsx
+++ pages/api/newsletter/index.tsx
@@ -1,12 +1,19 @@
 import express from "express";
+import { connect }  from '../../../mongoDB/connect';
 
-const handler = (req : express.Request, res : express.Response) => {
+const handler = async(req : express.Request, res : express.Response) => {
 if(req.method === "POST") {
     const {email}  = req.body;
 
     if(!email || !email.includes('@')) {
         return res.status(422).json({message:'invalid email address'}) ;
     }
+
+    const connectBb = await connect();
+    const db = connectBb.db();
+    await  db.collection('email').insertOne({emails: email});
+    connectBb.close();
+
 
     console.log(email)
     res.status(201).json({message:'ok'})
